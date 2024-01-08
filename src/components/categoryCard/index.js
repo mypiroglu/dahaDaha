66b0da141080a1ceb001bc6039838861b40d7f1a
Muhammet Yusuf Piroglu <muhammetyusufpiroglu@gmail.com>
2024-01-08 23:40:41 +0300

    update: categoryCard component

--- src/components/categoryCard/index.js
+++ src/components/categoryCard/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {Image, Pressable} from 'react-native';
 import {Text} from '..';
 import styles from './styles';
 
-export const CategoryCard = ({url, title, selected}) => {
+export const CategoryCard = ({url, title, selected, onPress}) => {
   return (
-    <View
+    <Pressable
+      onPress={onPress}
       style={
         selected ? [styles.container, {borderColor: 'red'}] : styles.container
       }>
       <Image source={{uri: url}} style={styles.image} />
       <Text style={styles.title}>{title}</Text>
-    </View>
+    </Pressable>
   );
 };
