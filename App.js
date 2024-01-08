37537b5ec8306689c61cd54676f009c2e4935603
Muhammet Yusuf Piroglu <muhammetyusufpiroglu@gmail.com>
2024-01-08 20:50:00 +0300

    feat: svg, images and colors add

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import CocaCola from './assets/svg/cocaCola.svg';
 
 const App = () => {
   return (
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,1 +1,5 @@
-export const Colors = {};
+export const Colors = {
+  red: '#FF0023',
+  black: '#1D1E1C',
+  green: '#009639',
+};
